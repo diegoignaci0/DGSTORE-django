198a06637b593a441fd4bbf11332870be01bd696
diegoignaci0 <diegoibarrabelmar@gmail.com>
2022-12-12 15:40:10 -0300

    Paginas implementadas

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, ejemplo
+from .views import index, nike, adidas, puma,login,signup
 
 
 urlpatterns = [
     path('', index,name="index"),
-    path('ejemplo/',ejemplo, name="ejemplo")
+    path('nike/',nike, name="nike"),
+    path('adidas/',adidas, name="adidas"),
+    path('puma/',puma, name="puma"),
+    path('login/',login, name="login"),
+    path('signup/',signup, name="signup"),
 ]
--- app/views.py
+++ app/views.py
@@ -5,6 +5,20 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'app/index.html')
 
+def nike(request):
+    return render(request, 'app/productos/nike.html')
 
-def ejemplo(request):
-    return render(request, 'app/ejemplo.html')
+def adidas(request):
+    return render(request, 'app/productos/adidas.html')
+
+def puma(request):
+    return render(request, 'app/productos/puma.html')
+
+
+def login(request):
+    return render(request, 'app/login.html')
+
+    
+def signup(request):
+    return render(request, 'app/signup.html')
+
