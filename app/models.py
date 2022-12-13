3bf7031eaf7140f0ca6846d5643ccb3e698a88af
diegoignaci0 <diegoibarrabelmar@gmail.com>
2022-12-13 10:00:23 -0300

    Models y media para subir imagenes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    stock = models.IntegerField()
+    imagen = models.ImageField(upload_to="productos",null=True)
+
+    def __str__(self):
+        return self.nombre
